91a38632ff8d00c2f67e677f26d7e254d1310e5c
Kamyar <kamyarkarimi98@gmail.com>
2021-12-27 18:43:21 -0500

    Search wikipedia api implemented

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,56 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
 	const [term, setTerm] = useState("");
+	const [results, setResults] = useState([]);
+	console.log(results);
 
 	useEffect(() => {
-		console.log("HELLO");
-	}, []);
+		const searchResult = async () => {
+			const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+				params: {
+					action: "query",
+					list: "search",
+					origin: "*",
+					format: "json",
+					srsearch: term,
+				},
+			});
+
+			setResults(data.query.search);
+		};
+
+		const timeoutId = setTimeout(() => {
+			if (term) {
+				searchResult();
+			}
+		}, 500);
+
+		return () => {
+			clearTimeout(timeoutId);
+		};
+	}, [term]);
+
+	const renderedResults = results.map((result) => {
+		return (
+			<div key={result.pageid} className="item">
+				<div className="right floated content">
+					<a
+						className="ui button"
+						href={`https://en.wikipedia.org?curid=${result.pageid}`}
+						target="_blank"
+					>
+						Go
+					</a>
+				</div>
+				<div className="content">
+					<div className="header">{result.title}</div>
+					<span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+				</div>
+			</div>
+		);
+	});
 
 	return (
 		<div>
@@ -19,6 +64,7 @@ const Search = () => {
 					/>
 				</div>
 			</div>
+			<div className="ui celled list">{renderedResults}</div>
 		</div>
 	);
 };
