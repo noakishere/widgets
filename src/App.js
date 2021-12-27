50877f26dad2cfd59dafd0b00a7f9498e8523f0a
Kamyar <kamyarkarimi98@gmail.com>
2021-12-27 17:15:02 -0500

    Initial search (useEffect)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
-  {
-    title: "What is a Seven Samurai?",
-    content: "A japanese movie made by Akira Kurosawa",
-  },
-  {
-    title: "Who's Akira Kurosawa?",
-    content: "A japanese movie director",
-  },
-  {
-    title: "Is Seven Samurai old?",
-    content: "ye somewhat",
-  },
+	{
+		title: "What is a Seven Samurai?",
+		content: "A japanese movie made by Akira Kurosawa",
+	},
+	{
+		title: "Who's Akira Kurosawa?",
+		content: "A japanese movie director",
+	},
+	{
+		title: "Is Seven Samurai old?",
+		content: "ye somewhat",
+	},
 ];
 
 export default () => {
-  return (
-    <div className="ui container">
-      <Accordion items={items} />
-    </div>
-  );
+	return (
+		<div className="ui container">
+			<Search />
+			{/* <Accordion items={items} /> */}
+		</div>
+	);
 };
