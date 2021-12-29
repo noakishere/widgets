1adc61863910edcd9945019b8fe64d6e505a23b6
Kamyar <kamyarkarimi98@gmail.com>
2021-12-28 20:09:47 -0500

    Initial dropdown list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 import Search from "./components/Search";
 
 const items = [
@@ -17,10 +18,32 @@ const items = [
 	},
 ];
 
+const options = [
+	{
+		label: "The color red",
+		value: "red",
+	},
+	{
+		label: "The color Green",
+		value: "green",
+	},
+	{
+		label: "A shade of Blue",
+		value: "blue",
+	},
+];
+
 export default () => {
+	const [selected, setSelected] = useState(options[0]);
+
 	return (
 		<div className="ui container">
-			<Search />
+			<Dropdown
+				selected={selected}
+				onSelectedChange={setSelected}
+				options={options}
+			/>
+			{/* <Search /> */}
 			{/* <Accordion items={items} /> */}
 		</div>
 	);
